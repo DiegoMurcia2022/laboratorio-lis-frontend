a29571dd8a7b1cb2248f8b32f32460d9db5121b9
DiegoMurcia2022 <diegomurciac2220@gmail.com>
2022-06-19 19:53:13 -0500

    route on App.js modify for GitHub-Pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/laboratorio-lis-frontend" element={<Home />} />
     </Routes>
   );
 }
